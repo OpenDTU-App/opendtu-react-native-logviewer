54265198a925335fab570e0c6106516b1de370fb
CommanderRedYT <ccommanderred@gmail.com>
2024-09-18 22:21:06 +0200

    Modify path via env

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
     },
-  },
+    build: {
+      sourcemap: true,
+    },
+    base: process.env.VITE_BASE_URL || '/',
+  };
 });
